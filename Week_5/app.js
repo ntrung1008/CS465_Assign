02ae658ba7f766e3f1d535d99b96bcc3404cd799
ntrung1008 <ntrung@pdx.edu>
2019-11-11 18:47:36 -0800

    try POST request

--- Week_5/app.js
+++ Week_5/app.js
@@ -1,7 +1,10 @@
 const express = require ('express');
-const path = require('path')
+var bodyParser=require("body-parser");
 
 const server = express();
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json());
 
 
 server.use(express.static('Week_5'));
@@ -10,6 +13,10 @@ server.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
   }); 
 
+server.post('/', function(req, res){
+   console.log(req.body);
+   res.end();}
+);
 
 server.listen(3000, function(){
     console.log("Listening on port 3000!")
